29caee7c7fea469676e94cc01682111a536748f8
andriyansyah0003 <andriyansyah0003@gmail.com>
2022-04-22 03:45:46 +0700

    init feature

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const buttonShortener = document.querySelector("#btn-shortener");
+buttonShortener.addEventListener("click", function () {
+  const url = document.querySelector("#url");
+  fetch("https://rama-shortner-backend.herokuapp.com/url/short", {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(url),
+    redirect: "follow",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("result").value = data.data["key"];
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
 function btnCopy() {
   const copyText = document.getElementById("result");
 
